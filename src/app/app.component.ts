3f9d674fe32b094247981ac359442f8803e31ff3
Daniel Leon <danielmatus468@gmail.com>
2024-01-11 01:27:57 -0600

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {FooterComponent} from "./components/footer/footer.component";
+import {DirectivesComponent} from "./components/directives/directives.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clientes-app';
+  title:string = 'clientes-app';
+  curso: string = 'Curso Spring con Angular';
+  profesor: string = 'Andrés Guzman';
 }
